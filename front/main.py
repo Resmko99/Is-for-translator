903383e27b22ac2168bde675cd1b789db30ce9ad
DaniilKOT112 <user1user2user3@mail.ru>
2024-02-15 21:19:21 +0300

    new

--- front/main.py
+++ front/main.py
@@ -1,13 +1,111 @@
-import sys
 from functools import partial
 from PySide6.QtCore import Qt, QPoint, QPropertyAnimation, QEasingCurve, QEvent
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QScrollArea, QHBoxLayout
+from PySide6.QtGui import QPixmap, QPainter
+
+import itertools
+
 from ui import Ui_MainWindow
 
+class RoundedImageLabel(QLabel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setContentsMargins(10, 10, 10, 10)  # Устанавливаем отступы для рамки
+        self.setStyleSheet("border-radius: 10px; border: 1px solid #FFCC33;")
+
+    def setPixmap(self, pixmap):
+        rounded_pixmap = self.rounded_pixmap(pixmap)
+        super().setPixmap(rounded_pixmap)
+
+    def rounded_pixmap(self, pixmap):
+        rounded_pixmap = QPixmap(pixmap.size())
+        rounded_pixmap.fill(Qt.transparent)
+        painter = QPainter(rounded_pixmap)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setBrush(Qt.white)
+        painter.setPen(Qt.NoPen)
+        painter.drawRoundedRect(pixmap.rect(), 10, 10)
+        painter.setCompositionMode(QPainter.CompositionMode_SourceIn)
+        painter.drawPixmap(0, 0, pixmap)
+        painter.end()
+        return rounded_pixmap
+
+
+class ImageScrollArea(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.scroll_area_contents_layout = QVBoxLayout(self)
+        self.scroll_area_contents_layout.setAlignment(Qt.AlignTop)
+
+        image_paths = [
+            r"Photo\AA1kTdiJ.jpg",
+            r"Photo\EyX_7safWuU.jpg",
+            r"Photo\x_89c4262c.jpg",
+            r"Photo\3G3tOpNoHPQ.jpg",
+            r"Photo\ee855f4a-8d70-4c03-b56e-f7a5059bbbec.jpg",
+            r"Photo\1bbb92772c6c2826a41f6f43dddb515d.jpg",
+            r"Photo\e1d2c368-d1eb-43f3-9dd6-c33c8ac680d2.jpg",
+            r"Photo\0a1a1fd8-f38f-4ce2-84a8-74a83c63203c.jpg",
+        ]
+
+        repeated_paths = itertools.cycle(image_paths)
+
+        self.scroll = QScrollArea()
+        self.scroll.setWidgetResizable(True)
+        self.scrollContent = QWidget()
+        self.scrollLayout = QVBoxLayout(self.scrollContent)
+        self.scroll.setStyleSheet("background-color: #24282E;"
+                                  "border: transparent;"
+                                  "border-radius: 20px;")
+
+        self.images_per_row = 5
+
+        for i in range(56):
+            if i % self.images_per_row == 0:
+                self.row_layout = QHBoxLayout()
+                self.scrollLayout.addLayout(self.row_layout)
+
+            self.image_text_container = QWidget()
+            self.image_text_container.setStyleSheet("background-color: #3D434B;"
+                                                    "border: none;"
+                                                    "border-radius: 10px;"
+                                                    "border: 2px solid #FFCC33;"
+                                                    "margin: 15px;")
+
+            self.image_text_layout = QVBoxLayout(self.image_text_container)
+
+            image_path = next(repeated_paths)
+            label = RoundedImageLabel()
+            pixmap = QPixmap(image_path)
+            pixmap = pixmap.scaled(250, 380, Qt.IgnoreAspectRatio)
+            if not pixmap.isNull():
+                label.setPixmap(pixmap)
+            else:
+                label.setText("Image not found")
+            label.setAlignment(Qt.AlignCenter)
+            label.setStyleSheet("border: none")
+            self.image_text_layout.addWidget(label)
+
+            text_label = QLabel(f"Text for Image {i + 1}")
+            self.image_text_layout.addWidget(text_label)
+            text_label.setStyleSheet("color: #fff; border: none; margin-top: 2px;")
+            text_label.setAlignment(Qt.AlignCenter)
+
+            self.row_layout.addWidget(self.image_text_container)
+
+        self.scrollLayout.addStretch()
+        self.scroll.setWidget(self.scrollContent)
+        self.scroll_area_contents_layout.addWidget(self.scroll)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.clicked_widget = None
+        self.image_scroll_area = None
+        self.layout = None
         self.resize_direction = None
         self.resize_offset = QPoint()
         self.drag_position = None
@@ -35,19 +133,49 @@ class MainWindow(QMainWindow):
             (self.ui.accountBtn_1, self.ui.accountBtn_2, self.ui.pageAccount),
             (self.ui.aboutUs_1, self.ui.aboutUs_2, self.ui.pageAboutUs),
             (self.ui.acceptFileBtn_1, self.ui.acceptFileBtn_2, self.ui.pageReceiveFile),
-            (self.ui.translateBtn_1, self.ui.translateBtn_2, self.ui.pageTranslator)
+            (self.ui.translateBtn_1, self.ui.translateBtn_2, self.ui.pageTranslator),
+
         ]
         for button, button_2, page in page_buttons:
             for btn in [button, button_2]:
                 btn.clicked.connect(partial(self.ui.stackedWidget_2.setCurrentWidget, page))
 
+        move_buttons = [
+            (self.ui.backAddTitleBtn, self.ui.pageTitle),
+            (self.ui.backBtnDesc, self.ui.pageTitle),
+            (self.ui.pushOpenAdd, self.ui.pageAddTitle)
+        ]
+
+        for button, page in move_buttons:
+            button.clicked.connect(partial(self.ui.stackedWidget_2.setCurrentWidget, page))
+
+
         self.ui.closeBtn.clicked.connect(self.closeApp)
         self.ui.expandBtn.clicked.connect(self.toggle_screen_state)
         self.ui.minimazeBtn.clicked.connect(self.minimizeApp)
-        self.screen_expanded = False
 
-        # Установка фильтра событий для главного окна
+        self.screen_expanded = False
         self.installEventFilter(self)
+        self.setup_scroll_area()
+
+    def setup_scroll_area(self):
+        self.image_scroll_area = ImageScrollArea()
+        self.ui.titleGrid.addWidget(self.image_scroll_area, 0, 0)
+
+        for child_widget in self.image_scroll_area.findChildren(RoundedImageLabel): # Добавление обработчиков щелчка мыши для открытия страницы self.ui.pageDesc
+            child_widget.mousePressEvent = lambda event, widget=child_widget: self.open_desc_page(event, widget)
+
+    def open_desc_page(self, event, widget):
+        if event.button() == Qt.LeftButton:
+            self.clicked_widget = widget  # Сохраняем информацию о нажатом виджете
+            self.update_photo_desc()
+            self.ui.stackedWidget_2.setCurrentWidget(self.ui.pageDesc)
+
+    def update_photo_desc(self):
+        if hasattr(self, 'clicked_widget'):  # Отображение закругленной картинки в photoDesc
+            pixmap = self.clicked_widget.pixmap() # Получаем pixmap из нажатого виджета
+            rounded_pixmap = self.clicked_widget.rounded_pixmap(pixmap)  # Получаем закругленный pixmap
+            self.ui.photoDesc.setPixmap(rounded_pixmap)  # Отображаем закругленную картинку в photoDesc
 
     def mouseDoubleClickEvent(self, event):
         if event.buttons() == Qt.LeftButton:
@@ -98,15 +226,15 @@ class MainWindow(QMainWindow):
         start_geometry = self.geometry()
         if not self.screen_expanded:
             end_geometry = QApplication.primaryScreen().availableGeometry()
-            self.normal_geometry = start_geometry  # Обновляем сохраненную геометрию
+            self.normal_geometry = start_geometry
         else:
-            end_geometry = self.normal_geometry  # Восстанавливаем начальную геометрию
+            end_geometry = self.normal_geometry
         if start_geometry == end_geometry:
             start_geometry, end_geometry = end_geometry, self.normal_geometry
 
         self.animation.setStartValue(start_geometry)
         self.animation.setEndValue(end_geometry)
-        self.animation.finished.connect(self.on_animation_finished)  # Связываем обработчик события завершения анимации
+        self.animation.finished.connect(self.on_animation_finished)
         self.animation.start()
 
         self.screen_expanded = not self.screen_expanded
@@ -114,7 +242,6 @@ class MainWindow(QMainWindow):
     def on_animation_finished(self):
         if not self.screen_expanded:
             self.setGeometry(self.normal_geometry)
-
 
     def mousePressEvent(self, event):
         if event.buttons() == Qt.LeftButton:
@@ -186,10 +313,10 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     with open("style.qss", "r") as style_file:
         style_str = style_file.read()
     app.setStyleSheet(style_str)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec())
+    app.exec()
