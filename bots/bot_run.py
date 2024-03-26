f237ead50236523115e0b0627a3d77f764205147
Resmko99 <daennae65@gmail.com>
2024-03-26 22:09:12 +0300

    Доработка телеграмм бота - сделана регистрация и выводы ошибки. Сделан тестовое подключение к гугл диску.

--- bots/bot_run.py
+++ bots/bot_run.py
@@ -8,6 +8,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import configparser
 from telebot import TeleBot
 
+# Парсим конфигурационный файл
 config = configparser.ConfigParser()
 config.read(os.path.join('src', 'config.ini'))
 bot_token = config['telegram']['token']
@@ -16,8 +17,10 @@ db_password = config['database']['password']
 db_host = config['database']['host']
 db_name = config['database']['name']
 
+# Создаем бота
 bot = TeleBot(bot_token)
 
+# Выполняем подключение к базе данных
 try:
     conn = psycopg2.connect(
         user=db_user,
@@ -29,118 +32,75 @@ try:
 except (Exception, psycopg2.Error) as error:
     print("Ошибка при подключении к PostgreSQL:", error)
 
+# Бот приветствует пользователя
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Сообщите мне свой логин, если вы забыли пароль.")
+    markup = InlineKeyboardMarkup()
+    itembtn1 = InlineKeyboardButton('Регистрация', callback_data="reg")
+    itembtn2 = InlineKeyboardButton('Восстановить пароль', callback_data="forgot")
+    markup.add(itembtn1, itembtn2)
 
-    # добавьте в конец файла/класса следующий код
+    bot.send_message(message.chat.id, "Привет! Выберите действие:", reply_markup=markup)
 
-def is_password_unique(password: str) -> bool:
+
+@bot.callback_query_handler(func=lambda call: call.data == "forgot")
+def forgot(call):
+    sent = bot.send_message(call.message.chat.id, 'Введите логин вашей учетной записи:')
+    bot.register_next_step_handler(sent, process_forgot_step)
+
+def process_forgot_step(message):
+    user_login = message.text
+    password = get_unique_password()
     try:
-        cursor.execute("SELECT password FROM users WHERE password=%s", (password,))
-        if cursor.fetchone():
-            return False
+        cursor.execute("UPDATE \"User\" SET password = %s WHERE login = %s", (password, user_login,))
+
+        conn.commit()
+        bot.send_message(message.chat.id, f"Ваш новый пароль: {password}")
     except (Exception, psycopg2.Error) as error:
         print("Ошибка при работе с PostgreSQL:", error)
+        bot.send_message(message.chat.id, "Произошла внутренняя ошибка при попытке обновить пароль. Повторите попытку.")
         conn.rollback()
 
-    return True
-
-
-def generate_password():
+def get_unique_password():
     while True:
-        password_length = 12
-        letters = string.ascii_letters
-        digits = string.digits
-        symbols = string.punctuation
-
-        password = [
-            random.choice(letters),
-            random.choice(digits),
-            random.choice(symbols),
-        ]
-
-        all_chars = letters + digits + symbols
-        for i in range(3, password_length):
-            password.append(random.choice(all_chars))
-
-        random.shuffle(password)
-        password = ''.join(password)
-
-        if is_password_unique(password):
+        password = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+        cursor.execute("SELECT * FROM \"User\" WHERE password = %s", (password,))
+        result = cursor.fetchone()
+        if not result:
             return password
 
-
-def handle_text(message):
-    user_login = message.text
-    new_password = generate_password()
-    try:
-        cursor.execute("SELECT * FROM users WHERE login = %s", (user_login,))
-        user = cursor.fetchone()
-        if user:
-            cursor.execute("UPDATE users SET password = %s WHERE login = %s", (new_password, user_login))
-            conn.commit()
-            bot.send_message(message.chat.id, f"Ваш новый пароль: {new_password}")
-        else:
-            bot.send_message(message.chat.id,
-                           "Пользователь с таким логином не найден. Пожалуйста, проверьте ваш логин.")
-    except (Exception, psycopg2.Error) as error:
-        print("Ошибка при работе с PostgreSQL:", error)
-        conn.rollback()
-
-
-@bot.message_handler(commands=['register'])
-def register(message):
-    msg = bot.send_message(message.chat.id, "Введите ваш логин:")
-    bot.register_next_step_handler(msg, process_login_step)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        if call.data == "reg":
+            sent = bot.send_message(call.message.chat.id, 'Введите ваш логин:')
+            bot.register_next_step_handler(sent, process_login_step)
 
 def process_login_step(message):
     user_login = message.text
-    try:
-        cursor.execute("SELECT * FROM users WHERE login = %s", (user_login,))
-        user = cursor.fetchone()
-        if user:
-            bot.send_message(message.chat.id, "Этот логин уже занят, попробуйте другой.")
-            return
-        else:
-            msg = bot.send_message(message.chat.id, 'Введите ваш пароль:')
-            bot.register_next_step_handler(msg, process_password_step, user_login)
-    except (Exception, psycopg2.Error) as error:
-        print("Ошибка при работе с PostgreSQL:", error)
-        bot.send_message(message.chat.id, "Произошла внутренняя ошибка при проверке логина. Попробуйте позже.")
-        conn.rollback()
+    cursor.execute("SELECT * FROM \"User\" WHERE login = %s", (user_login,))
+    result = cursor.fetchone()
+    if result:
+        bot.reply_to(message, 'Этот логин уже занят. Пожалуйста, введите другой логин.')
+    else:
+        sent = bot.send_message(message.chat.id, 'Введите ваш пароль:')
+        bot.register_next_step_handler(sent, process_password_step, user_login)
 
 def process_password_step(message, user_login):
     user_password = message.text
-    try:
-        # Transfer ownership of `is_password_unique` to this method to check password.
-        cursor.execute("SELECT password FROM users WHERE password=%s", (user_password,))
-        if cursor.fetchone():
-            bot.send_message(message.chat.id, "Этот пароль уже используется, попробуйте другой.")
-        else:
-            msg = bot.send_message(message.chat.id, 'Введите ваш email:')
-            bot.register_next_step_handler(msg, process_email_step, user_login, user_password)
-    except (Exception, psycopg2.Error) as error:
-        print("Ошибка при проверке уникальности пароля:", error)
-        bot.send_message(message.chat.id, "Произошла внутренняя ошибка при проверке пароля. Попробуйте позже.")
-        conn.rollback()
+    sent = bot.send_message(message.chat.id, 'Введите адрес вашей электронной почты:')
+    bot.register_next_step_handler(sent, process_email_step, user_login, user_password)
 
 def process_email_step(message, user_login, user_password):
     user_email = message.text
-    try:
-        cursor.execute("SELECT * FROM users WHERE email = %s", (user_email,))
-        user = cursor.fetchone()
-        if user:
-            bot.send_message(message.chat.id, "Этот email уже занят, попробуйте другой.")
-            return
-        else:
-            cursor.execute("INSERT INTO users (login, password, email, chat_id) VALUES (%s, %s, %s, %s)",
-                           (user_login, user_password, user_email, message.chat.id,))
-            conn.commit()
-            bot.send_message(message.chat.id, "Вы успешно зарегистрированы!")
-    except (Exception, psycopg2.Error) as error:
-        print("Ошибка при работе с PostgreSQL:", error)
-        bot.send_message(message.chat.id, "Произошла внутренняя ошибка при записи в базу данных. Попробуйте позже.")
-        conn.rollback()
+    cursor.execute("SELECT * FROM \"User\" WHERE email = %s", (user_email,))
+    result = cursor.fetchone()
+    if result:
+        bot.reply_to(message, 'Этот адрес электронной почты уже используется. Пожалуйста, введите другой.')
+    else:
+        cursor.execute("INSERT INTO \"User\" (login, password, email) VALUES (%s, %s, %s)",
+                       (user_login, user_password, user_email,))
+        conn.commit()
+        bot.send_message(message.chat.id, 'Вы успешно зарегистрировались!')
 
 bot.polling(none_stop=True)
